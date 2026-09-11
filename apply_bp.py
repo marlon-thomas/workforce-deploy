71ebd094e8c70d0f30925527376bc6879ea0c711
marlon-thomas <marlon-thomas@users.noreply.github.com>
2026-09-11 07:22:14 +0100

    chore: remove superseded scripts (setup-fedora, setup-local, dev-env-setup-commands, debug leftovers); setup.py reconciled with run.sh expectations (/opt paths when privileged)

--- apply_bp.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-sys.path.insert(0, "/authentik")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authentik.root.settings")
-import django
-django.setup()
-from authentik.blueprints.v1.importer import Importer
-with open("/blueprints/workforce-app.yaml") as f:
-    imp = Importer.from_string(f.read())
-print("APPLY:", imp.apply())
--- probe-db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /opt/workforce-deploy
-docker compose exec -T authentik-postgres psql -U authentik -d authentik -c "select slug from authentik_core_application" -c "select client_id from authentik_providers_oauth2_oauth2provider" 2>&1 | tail -6
